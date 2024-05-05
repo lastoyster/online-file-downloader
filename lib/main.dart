d74389409086e144230120f1e7f95a6cf379e349
lastoyster <kadampratiksha56@gmail.com>
2024-05-05 20:30:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:online file downloader/multi_select_demo.dart';
+import 'package:online file downloader/single_download.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SingleDownloadScreen());
+  }
+}
